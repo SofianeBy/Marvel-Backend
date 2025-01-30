f465a7762a346ee9b37f34756445a6b586974cbe
SofianeBy <sofiane.bouyerdene@outlook.fr>
2025-01-30 22:09:52 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json("Bienvenue Au PSG");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+app.get("/personage", (req, res) => {
+  try {
+    console.log("OUIIIII");
+    res.status(200).json("Personage !!!");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.all("*", (req, res) => {
+  try {
+    res.status(400).json("Not Found");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("SERVEUUUUR OOON");
+});
