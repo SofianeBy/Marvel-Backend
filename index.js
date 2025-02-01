26096b34d1bc4cc863c3a710a56094ca09c71d50
SofianeBy <sofiane.bouyerdene@outlook.fr>
2025-02-01 10:37:23 +0100

    4 commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    res.status(200).json("Bienvenue Au PSG");
+    const respose = await axios.get(
+      "https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=C9acOwqH8Qb8Vzmj",
+
+      res.status(200).json("respose")
+    );
   } catch (error) {
     console.log(error.message);
   }
